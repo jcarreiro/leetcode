cd954ae4ce917472446c6ad48c5b4e1f0707e280
Jason Carreiro <jcarreiro@meta.com>
2025-07-16 16:18:25 -0400

    Add solution template

---
+++ template/solution.h
@@ -0,0 +1,8 @@
+#include <gtest/gtest.h>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+  // add method here
+};
---
+++ template/solution_test.cc
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include <random>
+#include <stack>
+#include <unordered_set>
+#include "solution.h"
+
+using namespace std;
+
+TEST(SolutionTest, Test01) {
+  // Add test case(s) here.
+  EXPECT_TRUE(false);
+}
