1036e6b7ff1429c8c259bf61e48589c13b89a8f6
Jason Carreiro <jcarreiro@meta.com>
2025-08-14 13:06:03 -0400

    [int150] Is subsequence

---
+++ 0392_is_subsequence/solution_test.cc
@@ -0,0 +1,18 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include <random>
+#include <stack>
+#include <unordered_set>
+#include "solution.h"
+
+using namespace std;
+
+TEST(SolutionTest, Test01) {
+  Solution s;
+  EXPECT_TRUE(s.isSubsequence("abc", "ahbgdc"));
+}
+
+TEST(SolutionTest, Test02) {
+  Solution s;
+  EXPECT_FALSE(s.isSubsequence("axc", "ahbgdc"));
+}
